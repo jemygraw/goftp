d5f438d171aaa82ff08af39ac60bac3bc59a99e5
Jemy Graw <jemygraw@gmail.com>
2014-03-24 14:05:05 +0800

    为gocount添加注释
    1. 为gocount添加注释

--- src/main/gocount.go
+++ src/main/gocount.go
@@ -10,11 +10,15 @@ import (
 	"strings"
 )
 
+//定义gocount命令的参数
+// -d 参数 指定项目的根目录，默认为命令启动时的目录
+// -c 参数 指定是否需要计算注释的行数，默认为计算
 var (
 	dirFlag = flag.String("d", ".", "项目根目录")
 	cmtFlag = flag.Bool("c", true, "包含注释")
 )
 
+//定义go语言的注释标记和文件后缀名
 const (
 	LINE_COMMENT_START_FLAG  string = `//`
 	BLOCK_COMMENT_START_FLAG string = `/*`
@@ -23,12 +27,14 @@ const (
 	GO_FILE_SUFFIX string = ".go"
 )
 
+//为每个文件定义一个GoFileLineCount结构体
 type GoFileLineCount struct {
-	CodeCount         int
-	LineCommentCount  int
-	BlockCommentCount int
+	CodeCount         int //代码行数
+	LineCommentCount  int //行注释行数
+	BlockCommentCount int //块注释行数
 }
 
+//获取该文件的内容总行数
 func (this GoFileLineCount) totalLines() int {
 	return this.CodeCount + this.LineCommentCount + this.BlockCommentCount
 }
@@ -40,27 +46,27 @@ func countProject(projectDir string, withComment bool) {
 			var totalCodeCount int = 0
 			var totalLineCommentCount int = 0
 			var totalBlockCommentCount int = 0
+
 			filepath.Walk(projectDir, func(path string, info os.FileInfo, err error) (retError error) {
 				fileInfo, fileInfoErr := os.Stat(path)
 				if fileInfoErr == nil {
 					if !fileInfo.IsDir() && strings.HasSuffix(strings.ToLower(path), GO_FILE_SUFFIX) {
-
 						fileLineCount, countErr := countFile(path)
 						if countErr != nil {
-							fmt.Println(path, ": Count Error")
+							fmt.Println(path, ": 计算错误")
 						} else {
 							totalCodeCount += fileLineCount.CodeCount
 							totalLineCommentCount += fileLineCount.LineCommentCount
 							totalBlockCommentCount += fileLineCount.BlockCommentCount
 						}
-
 					}
 				} else {
 					retError = fileInfoErr
-					fmt.Println(path, ": Access Error")
+					fmt.Println(path, ": 无法访问")
 				}
 				return
 			})
+
 			fmt.Println()
 			if withComment {
 				fmt.Println("行注释:", totalLineCommentCount)
@@ -77,13 +83,14 @@ func countProject(projectDir string, withComment bool) {
 				fmt.Println()
 			}
 		} else {
-			fmt.Println("Can not access directory", projectDir)
+			fmt.Println("无法访问目录:", projectDir)
 		}
 	} else {
 		fmt.Println(err)
 	}
 }
 
+//获取每个文件的内容行数
 func countFile(path string) (fileLineCount GoFileLineCount, err error) {
 	file, fileError := os.Open(path)
 	if fileError == nil {
@@ -93,7 +100,7 @@ func countFile(path string) (fileLineCount GoFileLineCount, err error) {
 		for {
 			lineData, readErr := bReader.ReadString('\n')
 			lineData = strings.TrimSpace(lineData)
-
+			//分别计算文件中代码，块注释和行注释的总行数
 			if strings.HasPrefix(lineData, LINE_COMMENT_START_FLAG) {
 				fileLineCount.LineCommentCount++
 			} else if strings.HasPrefix(lineData, BLOCK_COMMENT_START_FLAG) {
